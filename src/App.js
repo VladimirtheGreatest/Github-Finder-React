4cf86f6c4197e888e2ed158bf2bbda3d3777075e
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-09-18 19:57:17 +0100

    added router and links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, {Component} from 'react';
+import React, { Fragment,Component} from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
 import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
+import About from './components/pages/About'
 import axios from 'axios'
 import './App.css';
 
@@ -34,19 +36,28 @@ setAlert = (msg, type) => {
   render(){
     const { loading, users } = this.state;
   return (
+    <Router>
     <div className="App">
      <Navbar  />
      <div className='container'>
      <Alert alert={this.state.alert} />
-     <Search 
+     <Switch>
+       <Route exact path='/' render={props => (
+         <Fragment>
+           <Search 
      searchUsers={this.searchUsers} 
      clearUsers={this.clearUsers} //clearUsers props from Search.js on Click function, this will fireoff clearUsers and will empty the array
       showClear={this.state.users.length > 0 ? true : false}  //if array is empty we will not show the clear button
       setAlert={this.setAlert}
      />
      <Users loading={loading} users={users} />
+         </Fragment>
+       )}/>
+       <Route exact path='/about' component={About} />
+     </Switch>
      </div>
     </div>
+    </Router>
   );
  }
 }
