740eefddd3229ee6f8ed705e43d4069bcccb2b98
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-10-04 21:54:53 +0100

    converting class based components to function based with useState hook

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,47 +1,44 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
-export class Search extends Component {
-    state = {
-        text: ''
-    }
+const Search = ({searchUsers, showClear, clearUsers, setAlert}) => {
+    const [text, setText] = useState('')  //hook state
 
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        setAlert:PropTypes.func.isRequired
-    }
 
-    onChange = e => this.setState({ [e.target.name]: e.target.value })
+    const onChange = e => setText(e.target.value )
     
-    onSubmit = e => {
+     const onSubmit = e => {
         e.preventDefault();
-        if (this.state.text === '') {
-         this.props.setAlert('Please enter something', 'light')
+        if (text === '') {
+         setAlert('Please enter something', 'light')
         } else {
-            this.props.searchUsers(this.state.text);
-            this.setState({text: ''});
+            searchUsers(text);
+            setText('');
         }
     };
-
-    render() {
-        const {clearUsers, showClear} = this.props;  //destructuring instead of this.props.clearUsers and this.props.showClear
+    //no longer need this. keyword since I am using hooks and there is no class
         return (
             <div>
-                <form onSubmit={this.onSubmit} className="form">
+                <form onSubmit={onSubmit} className="form"> 
                 <input type="text" 
                 name="text" 
                 placeholder="Search Users..." 
-                value={this.state.text}
-                onChange={this.onChange}
+                value={text}
+                onChange={onChange}
                  />
                 <input type="Submit" name="Search" className="btn btn-dark btn-block" /> 
                 </form>
                 {showClear && (<button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>)}
             </div>//clear button thats visible only if meet certain condition, check app.js for conditions
         )
-    }
+    
+}
+
+Search.propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert:PropTypes.func.isRequired
 }
 
 export default Search
