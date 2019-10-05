8071781165d94ceb34b61902589d958f3e0a452e
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-10-05 19:12:29 +0100

    changing the user class based component to the functional based component, adding useEffect hooks to mimic the ComponentdidMount behaviour

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,23 +1,16 @@
-import React, { Fragment, Component } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import Spinner from '../layout/spinner'
 import Repos from '../repos/Repos'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-export class User extends Component {
-    componentDidMount() {
-this.props.getUser(this.props.match.params.login)
-this.props.getUserRepos(this.props.match.params.login)
-    }
+const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
+    useEffect(() => {
+        getUser(match.params.login)
+        getUserRepos(match.params.login)
+        //eslint-disable-next-line
+    }, []) //in order to mimic the behaviour of ComponentdidMount we put the empty brackets here
 
-    static propTypes = {
-        loading:PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        repos: PropTypes.array.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos:PropTypes.func.isRequired
-    }
-    render() {
         const {
             name,
             company,
@@ -32,9 +25,8 @@ this.props.getUserRepos(this.props.match.params.login)
             public_repos,
             public_gists,
             hireable
-        } = this.props.user;
+        } = user;
 
-        const { loading, repos }  = this.props;
 
         if(loading) return <Spinner />
         return (
@@ -102,7 +94,14 @@ this.props.getUserRepos(this.props.match.params.login)
                <Repos repos={ repos } />
             </Fragment>
         )
-    }
+}
+
+User.propTypes = {
+    loading:PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos:PropTypes.func.isRequired
 }
 
 export default User
