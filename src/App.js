ec73db1ac77050a0c51baf8f2bd1b38b2e163c51
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-10-09 23:43:10 +0100

    added context reducer and converting app.js state to context state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
 import About from './components/pages/About'
 import axios from 'axios'
+import GithubState from './context/github/GithubState'
 import './App.css';
 
 const App = () =>  {
@@ -61,6 +62,7 @@ const App = () =>  {
 
   
   return (
+    <GithubState>
     <Router>
     <div className="App">
      <Navbar  />
@@ -92,6 +94,7 @@ const App = () =>  {
      </div>
     </div>
     </Router>
+    </GithubState>
   )
 }
 
