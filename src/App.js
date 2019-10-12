ab9b3f62a1dab0451658d959c84457fedff4ab79
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-10-12 23:59:27 +0100

    added users Github context

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ const App = () =>  {
       showClear={users.length > 0 ? true : false}  //if array is empty we will not show the clear button
       setAlert={showAlert}
      />
-     <Users loading={loading} users={users} />
+     <Users />
          </Fragment>
        )}/>
        <Route exact path='/about' component={About} />
